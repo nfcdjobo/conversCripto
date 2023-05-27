76958b9450d57f50680bf909ab09700d65eb4d52
nfcdjobo <nfcdjobo@gmail.com>
2023-05-27 11:55:01 +0000

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const apiKey = 'e26e4924ff9842733c5d3f1dec0748e40b60265ee80e6a231b55ad7bd09df5f8';
+let crypto1 = document.getElementById('c')
+const url = `https://min-api.cryptocompare.com/data/price?fsym=${crypto1}&tsyms=${crYpto2}&api_key=${apiKey}`;
+
+fetch(url).then(response => {
+    response.
+})
